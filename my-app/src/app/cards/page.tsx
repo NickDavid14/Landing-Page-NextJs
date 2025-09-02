27f2a4a78bed812d3b810346523df332255b32c4
nicodoido <nickdavid1407@gmail.com>
2025-09-01 23:03:28 -0300

    cards adicionados e alterados

--- my-app/src/app/cards/page.tsx
+++ my-app/src/app/cards/page.tsx
@@ -18,7 +18,7 @@ export default function WobbleCardDemo() {
   <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full mt-10">
   
     {/* Terceiro card (agora o de baixo): Stacks com carrossel */}
-    <WobbleCard containerClassName="w-full col-span-1 lg:col-span-3 bg-amber-3100">
+    <WobbleCard containerClassName="w-full col-span-1 lg:col-span-3 bg-red-290">
 
       <div className="flex flex-col items-center justify-center h-full">
         <h2 className="text-center text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-black font-serif mb-5">
@@ -28,7 +28,7 @@ export default function WobbleCardDemo() {
           items={stacks}
           direction="right"
           speed="fast"
-          
+          className=""
         />
       </div>
     </WobbleCard>
